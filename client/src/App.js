8e979254958fad7c9ca7fe09ae4e9470e4da9af9
David Reyes-Cairo <dreyescairo@gmail.com>
2018-10-25 21:20:09 -0600

    created svg repository. pass svg as prop to logoHeader.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import LogoHeader from './components/layout/LogoHeader';
-import logo from '../src/images/SignatureVectorV2.png';
+import LogoHeader from "./components/layout/LogoHeader";
+import logo from "../src/images/SignatureVectorV2.png";
+import { SvgRepository } from "./components/svgs/svgRepository";
 class App extends Component {
-
   constructor(props) {
+    let svgRepo = new SvgRepository();
+    let svgLogo = svgRepo.getLogoSvg();
+
     super(props);
     this.state = {
-
-
+      svgLogo: svgLogo
     };
   }
-
-
 
   render() {
     return (
@@ -21,32 +21,41 @@ class App extends Component {
         <Navbar />
 
         <div className="container">
-
           <div className="col s6">
-            <LogoHeader imgPath={logo} />
-
-
+            <LogoHeader imgPath={logo} svgLogo={this.state.svgLogo} />
           </div>
-
 
           <div className="row">
             <div className="col s12 m7">
               <div className="card small">
                 <div className="card-image waves-effect waves-block waves-light">
-                  <img className="activator" src="https://designcode.io/images/react-landing-page/react-code-3.png" />
+                  <img
+                    className="activator"
+                    src="https://designcode.io/images/react-landing-page/react-code-3.png"
+                  />
                 </div>
                 <div className="card-content">
-                  <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-                  <p><a href="#">This is a link</a></p>
+                  <span className="card-title activator grey-text text-darken-4">
+                    Card Title
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                  <p>
+                    <a href="#">This is a link</a>
+                  </p>
                 </div>
                 <div className="card-reveal">
-                  <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-                  <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                  <span className="card-title grey-text text-darken-4">
+                    Card Title
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     );
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 
-
 class Navbar extends Component {
-
   render() {
-
-
-
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Logo</a>
+          <a href="#" className="brand-logo">
+            Logo
+          </a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
           </ul>
         </div>
       </nav>
